076c89c45d37164c4723e20b3e6df2b49eb2f169
Aravind Sankaran <sankaran@aices.rwth-aachen.de>
2022-08-09 19:37:08 +0200

    Added violin plot to measurement visualizer

--- algorithm_ranking/measurements_visualizer.py
+++ algorithm_ranking/measurements_visualizer.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from matplotlib.ticker import FormatStrFormatter
+import numpy as np
 
 class MeasurementsVisualizer:
     def __init__(self, alg_measurements, alg_seq_h0=None):
@@ -8,6 +9,14 @@ class MeasurementsVisualizer:
         self.alg_seq_h0 = alg_seq_h0
         if not alg_seq_h0:
             self.alg_seq_h0 = list(self.measurements.keys())
+
+    def remove_outliers(self,x):
+        x = np.array(x)
+        q1, q2 = np.percentile(x, [25, 75])
+        iqr = q2 - q1
+        fence_low = q1 - 1.5 * iqr
+        fence_high = q2 + 1.5 * iqr
+        return list(x[(x > fence_low) & (x < fence_high)])
 
     def show_measurement_histograms(self, alg_list=None, bins=10, hspace=0.5):
         if not alg_list:
@@ -84,6 +93,73 @@ class MeasurementsVisualizer:
 
         # y-axis labels
         ax.set_yticklabels(y)
+
+        # Removing top axes and right axes
+        # ticks
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+
+        plt.show()
+
+    def show_measurements_violinplot(self, alg_list=None, outliers=False, scale=1.5):
+        if not alg_list:
+            alg_list = self.alg_seq_h0
+
+        fig = plt.figure(figsize=(10, scale * len(alg_list)))
+        ax = fig.add_subplot(111)
+
+        x_b = []
+        x_v = []
+        y = []
+        for alg in alg_list:
+            data = self.remove_outliers(self.measurements[alg])
+            x_v.append(data)
+            x_b.append(self.measurements[alg])
+            y.append(alg)
+
+        # print(x)
+
+        bp = ax.boxplot(x_b, patch_artist=True,
+                        notch=False, vert=0, showfliers=outliers,
+                        positions=range(len(y)))
+
+        # # Creating axes instance
+        vp = ax.violinplot(x_v, showmedians=False, vert=False, showextrema=False,
+                           positions=range(len(y)))
+
+        x_lim = ax.get_xlim()
+
+        for i, data in enumerate(x_b):
+            sp = ax.plot(data, np.ones(len(data)) * i, 'b.', alpha=0.9)
+            ax.set_xlim(x_lim)
+
+        colors = ['#E9EFF5'] * len(y)
+        edge_color = '#9FBCF5'
+
+        for patch, color in zip(bp['boxes'], colors):
+            plt.setp(patch, color=edge_color)
+            patch.set_facecolor(color)
+
+        # changing color and linewidth of
+        # whiskers
+        for whisker in bp['whiskers']:
+            whisker.set(color='#8B008B',
+                        linewidth=1.5,
+                        linestyle=":")
+
+        # changing color and linewidth of
+        # caps
+        for cap in bp['caps']:
+            cap.set(color='#8B008B',
+                    linewidth=2)
+
+        # changing color and linewidth of
+        # medians
+        for median in bp['medians']:
+            median.set(color='red',
+                       linewidth=2)
+
+        ax.set_yticks(range(len(y)), labels=y)
 
         # Removing top axes and right axes
         # ticks
