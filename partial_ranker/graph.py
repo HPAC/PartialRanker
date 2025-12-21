6ce09edc7b9be6c4146ef71311208ff79dabcddc
sankaran2 <sankaran2@jsfl01.jusuf>
2025-12-21 19:44:23 +0100

    Changed licence

--- partial_ranker/graph.py
+++ partial_ranker/graph.py
@@ -1,21 +1,11 @@
 # Partial Ranker
 #
-# Copyright (C) 2019-2024, Aravind Sankaran
-# IRTG-2379: Modern Inverse Problems, RWTH Aachen University, Germany
-# HPAC, Umeå University, Sweden
+# Copyright (c) 2019-2025, High-Performance and Automatic Computing group
+# at RWTH Aachen University and Umeå University.
+# All rights reserved.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Licensed under the BSD 3-Clause License.
+# See LICENSE file in the project root for full license information.
 #
 # Contributors:
 # - Aravind Sankaran
